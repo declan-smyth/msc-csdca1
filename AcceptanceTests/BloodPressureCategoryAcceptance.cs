41aa84034743d004131e00ad45950857b3ae5dfb
Declan Smyth <declan.smyth@gmail.com>
2018-11-19 22:58:07 +0000

    Add new Selenium Tests to test release pipeline

---
+++ AcceptanceTests/BloodPressureCategoryAcceptance.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+
+namespace BPCalculator.AcceptanceTest.BloodPressure
+{
+    [TestClass]
+    public class BloodPressureCategoryAcceptance
+    {
+        private IWebDriver driver;
+        private string appURL;
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void TestSetup()
+        {
+            appURL = "https://bpcalculator-dev-as-dev.azurewebsites.net";
+
+            string browser = "Firefox";
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver(Environment.GetEnvironmentVariable("GekoWebDriver"));
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver(Environment.GetEnvironmentVariable("IEWebDriver"));
+                    break;
+                default:
+                    driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
+                    break;
+            }
+
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            driver.Quit();
+        }
+
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void TheBingSearchTest()
+        {
+            driver.Navigate().GoToUrl(appURL + "/");
+            driver.FindElement(By.Id("sb_form_q")).SendKeys("Azure Pipelines");
+            driver.FindElement(By.Id("sb_form_go")).Click();
+            driver.FindElement(By.XPath("//ol[@id='b_results']/li/h2/a/strong[3]")).Click();
+
+            Assert.IsTrue(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
+        }
+    }
+}
--- SeleniumTests/SeleniumUnitTests.cs
+++ SeleniumTests/SeleniumUnitTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 using System.Security.Permissions;
 
 // xUnit Test Framework
@@ -6,16 +8,15 @@ using Xunit;
 
 // Selenium Test Framework
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
+
 
 
 //using OpenQA.Selenium.PhantomJS;
 
-
-
-namespace SeleniumTests
+namespace BPCalculator.AcceptanceTest.BloodPressure
 {
     public class SeleniumUnitTests : IDisposable
     {
@@ -30,10 +31,11 @@ namespace SeleniumTests
                 // Setup the browser to use during the testing
                 ChromeOptions options = new ChromeOptions();
                 //options.AddArgument("--headless");
-               // webDriver = new ChromeDriver(options);
-                webDriver = new FirefoxDriver();
+                
 
-                // Get the WebappURI
+                // Setup Chrome Driver 
+               //webDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                
 
             }
             catch (Exception e)
@@ -50,7 +52,7 @@ namespace SeleniumTests
         {
             try
             {
-                webDriver.Quit();
+               // webDriver.Quit();
             
             }
             catch (Exception e)
@@ -62,29 +64,36 @@ namespace SeleniumTests
         [Fact]
         public void TestSystolicMaxValue()
         {
-            webDriver = new FirefoxDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
 
-            // Navigate to the page
-            webDriver.Navigate().GoToUrl("www.google.com");
-            
-            IWebElement query = webDriver.FindElement(By.Name("q"));
-            query.SendKeys("Cheese");
-            System.Console.WriteLine("Page title is: " + webDriver.Title);
-            webDriver.Quit();
+            if (webDriver == null)
+            {
+                // Setup Chrome Driver 
+                webDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            }
 
+            webDriver.Manage().Window.Maximize();
 
+            webDriver.Navigate().GoToUrl(@"https://en.wikipedia.org/wiki/Main_Page");
 
+            Assert.Equal("Wikipedia, the free encyclopedia", webDriver.Title);
+           
+        }
 
-            // Get the Systolic Element
-            //IWebElement systolicElement = webDriver.FindElement(By.Id("BP.Systolic"));
-
-            //Get the Distoloic Element
-            //IWebElement diastolicElement = webDriver.FindElement(By.ClassName(""))
-
-            // Send 10 to Systolic Element
-            // systolicElement.SendKeys("60");
-
-
+        [Fact]
+        public void TempTest()
+        {
+            //using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+            //{
+            //    driver.Navigate().GoToUrl(@"https://automatetheplanet.com/multiple-files-page-objects-item-templates/");
+            //    var link = driver.FindElement(By.PartialLinkText("TFS Test API"));
+            //    var jsToBeExecuted = $"window.scroll(0, {link.Location.Y});";
+            //    ((IJavaScriptExecutor)driver).ExecuteScript(jsToBeExecuted);
+            //    var wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+            //    var clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("TFS Test API")));
+            //    clickableElement.Click();
+            //}
         }
     }
 }
